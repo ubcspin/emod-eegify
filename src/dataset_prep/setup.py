fecd6b3167bfc11ae97914b5f6b2da4010f4a0d4
bguta <bereket17@hotmail.com>
2023-02-28 15:25:26 -0800

    update train/val

--- src/dataset_prep/setup.py
+++ src/dataset_prep/setup.py
@@ -25,6 +25,7 @@ OUTPUT_DIR = 'COMBINED_DATA'
 OUTPUT_PICKLE_NAME = 'subject_data.pk'
 SAVE_PICKLE_FILE = True
 
+print("Reading data...")
 read_data.run(RAW_DATA_PATH, OUTPUT_DIR, FILES_DICT, OUTPUT_PICKLE_NAME, SAVE_PICKLE_FILE)
 
 
@@ -44,6 +45,7 @@ FILE_ORDER = [
     'calibrated_words.csv'
 ]
 
+print("Cleaning data...")
 clean_data.run(INPUT_PICKLE_FILE, INPUT_DIR, INPUT_PICKLE_NAME, SAVE_PICKLE_FILE,
                OUTPUT_DIR, OUTPUT_PICKLE_NAME, FILE_ORDER, SAMPLE_PERIOD,
                FS, MAX_CONTINUOUS_ANNOTATION, DEBUG)
@@ -59,15 +61,20 @@ SAVE_PICKLE_FILE = True
 OUTPUT_DIR = 'COMBINED_DATA'
 OUTPUT_PICKLE_NAME = 'featurized_data.pk'
 
+
+print("Calculating features...")
 calculate_features.run(INPUT_PICKLE_FILE, INPUT_DIR, INPUT_PICKLE_NAME, SAVE_PICKLE_FILE,
         OUTPUT_DIR, OUTPUT_PICKLE_NAME, FS, TIME_INDEX, EXP_PARAMS)
 
 
 import calculate_labels
+print("Calculating labels...")
 calculate_labels.run()
 
 import calculate_validation
+print("Creating validation split...")
 calculate_validation.run()
 
 
+print("Done!")
 sys.path.remove(str(_parentdir))
--- src/validation/val_hc.py
+++ src/validation/val_hc.py
@@ -1,3 +1,9 @@
+import pathlib
+import sys
+
+_parentdir = pathlib.Path(__file__).parent.parent.resolve()
+sys.path.insert(0, str(_parentdir))
+
 import os
 import utils
 import numpy as np
@@ -7,9 +13,9 @@ from tqdm import tqdm
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import make_scorer
 
-from val_estimator_helper_hc import EstimatorSelectionHelper
+from validation.val_estimator_helper_hc import EstimatorSelectionHelper
 
-from models_val import MODELS, PARAMS
+from validation.models_val import MODELS, PARAMS
 from config import EXP_PARAMS
 
 from hiclass2 import metrics
@@ -18,8 +24,8 @@ from hiclass2 import metrics
 
 INPUT_PICKLE_FILE = True
 INPUT_DIR = 'COMBINED_DATA'
-INPUT_PICKLE_NAME = '_val_featurized_data.pk'
-INPUT_LABEL_NAME = '_val_labels.pk'
+INPUT_PICKLE_NAME = '_featurized_data.pk'
+INPUT_LABEL_NAME = '_labels.pk'
 
 SAVE_PICKLE_FILE = True
 OUTPUT_DIR = 'RESULTS'
@@ -33,47 +39,61 @@ LABEL_TYPES = [
 
 
 
-def fit_helper(X, y, models=MODELS, params=PARAMS, n_jobs=-1, scoring={ "f1": make_scorer(metrics.f1), "prec" : make_scorer(metrics.precision), "recall": make_scorer(metrics.recall)}, cw_classes=None):
+def fit_helper(X, y, X_test, y_test, models=MODELS, params=PARAMS, n_jobs=-1, scoring={ "f1": make_scorer(metrics.f1), "prec" : make_scorer(metrics.precision), "recall": make_scorer(metrics.recall)}, cw_classes=None):
     helper = EstimatorSelectionHelper(models, params, cw_classes)
-    helper.fit(X, y, scoring=scoring, n_jobs=n_jobs)
+    helper.fit(X, y,  X_test, y_test, scoring=scoring, n_jobs=n_jobs)
     try:
-        scores = helper.score_summary(X, sort_by='mean_test_f1')
+        scores = helper.score_summary(X_test, sort_by='mean_test_f1')
         return helper, scores
     except ValueError:
         return -1, -1
 
 
-def train(feature_dict, label_dict, label_types=LABEL_TYPES, feature_types=FEATURE_TYPES):
+def train(feature_dict, label_dict, test_feature_dict, test_label_dict, 
+          label_types=LABEL_TYPES, feature_types=FEATURE_TYPES, pnum='p01'):
+
 
     # read the features back into a numpy array
 
-    all_bands = np.array([x[feature_types].to_numpy() for x in feature_dict])
-    all_features = []
-    for bands in all_bands:
-        features = np.empty((bands.shape[0], bands.shape[1], 64, 64))
-        for band in range(len(feature_types)):
-            band_arr = bands[:,band]
-            for window in range(band_arr.shape[0]):
-                band_im = np.array(band_arr[window])
-                features[window, band, :, :] = band_im
-        all_features.append(features.astype(np.float32))
+    # read the features back into a numpy array
+    bands = feature_dict[feature_types].to_numpy()
+    features = np.empty((bands.shape[0], bands.shape[1], 64, 64))
+    for band in range(len(feature_types)):
+        band_arr = bands[:,band]
+        for window in range(band_arr.shape[0]):
+            band_im = np.array(band_arr[window])
+            features[window, band, :, :] = band_im
+    X = features.astype(np.float32)
 
-    X = np.vstack(all_features)
-    print(X.shape)
-    res = {}
+    # read the features back into a numpy array
+    bands = test_feature_dict[feature_types].to_numpy()
+    features = np.empty((bands.shape[0], bands.shape[1], 64, 64))
+    for band in range(len(feature_types)):
+        band_arr = bands[:,band]
+        for window in range(band_arr.shape[0]):
+            band_im = np.array(band_arr[window])
+            features[window, band, :, :] = band_im
+    X_test = features.astype(np.float32)
 
     Y = label_dict
+    Y_test = test_label_dict
 
     LE = LabelEncoder() # transform string to class values
-    LE.fit(Y['cw_mode'])
+    LE.fit(np.vstack( (Y['cw_mode'], Y_test['cw_mode'])  ))
 
     y_cw_str = Y['cw_mode']
     y_cw = LE.transform(y_cw_str)
+
+    y_cw_str_test = Y_test['cw_mode']
+    y_cw_test = LE.transform(y_cw_str_test)
 
     for label_type in label_types: # train every label type
 
         y = np.array(list(zip(y_cw, Y[label_type])))
         y_str = np.array(list(zip(y_cw_str, Y[label_type] )))
+
+        y_test = np.array(list(zip(y_cw_test, Y_test[label_type])))
+        y_str_test = np.array(list(zip(y_cw_str_test, Y_test[label_type] )))
 
         res['y_hc_' + label_type] = y_str
         helper, scores = fit_helper(X, y, cw_classes=len(LE.classes_))
@@ -85,25 +105,36 @@ def train(feature_dict, label_dict, label_types=LABEL_TYPES, feature_types=FEATU
 
 
 if __name__ == '__main__':
-    for window_size in [500]:
-        if INPUT_PICKLE_FILE:
-            results = {}
-            input_label_file_path = os.path.join(INPUT_DIR, str(window_size) + 'ms' + INPUT_LABEL_NAME)
-            labels = utils.load_pickle(pickled_file_path=input_label_file_path)
+    for i in range(100, 101):
+        for window_size in EXP_PARAMS['WINDOW_SIZE']:
+            if INPUT_PICKLE_FILE:
+                participant_results = {}
+                for subject_id in tqdm(SUBJECT_IDS):
+                    utils.logger.info(f'Training participant {subject_id}')
+
+                    training_data_filename = subject_id + "_" + str(window_size) + 'ms' + INPUT_PICKLE_NAME
+                    testing_data_filename = subject_id + "_" + str(window_size) + 'ms' + "_val" + INPUT_PICKLE_NAME
+
+                    input_pickle_file_path_train = os.path.join(INPUT_DIR, training_data_filename)
+                    input_pickle_file_path_test = os.path.join(INPUT_DIR, testing_data_filename)
+
+                    input_label_file_path_train = os.path.join(INPUT_DIR, str(window_size) + 'ms' + INPUT_LABEL_NAME)
+                    input_label_file_path_test = os.path.join(INPUT_DIR, str(window_size) + 'ms' + "_val" + INPUT_LABEL_NAME)
+
+                    train_features = utils.load_pickle(pickled_file_path=input_pickle_file_path_train)
+                    train_labels = utils.load_pickle(pickled_file_path=input_label_file_path_train)
+                    
+                    test_features = utils.load_pickle(pickled_file_path=input_pickle_file_path_test)
+                    test_labels = utils.load_pickle(pickled_file_path=input_label_file_path_test)
 
 
-            all_labels = [labels[x] for x in SUBJECT_IDS]
-            labels = pd.concat(all_labels)
+                    utils.logger.info(f'Window size {window_size} - Iteration {i}')
+                    results = train(train_features[subject_id], train_labels[subject_id], test_features[subject_id], test_labels[subject_id], pnum=subject_id)
 
-            all_features = []
-            for subject_id in tqdm(SUBJECT_IDS):
-                training_data_filename = subject_id + "_" + str(window_size) + 'ms' + INPUT_PICKLE_NAME
-                input_pickle_file_path = os.path.join(INPUT_DIR, training_data_filename)
-                features = utils.load_pickle(pickled_file_path=input_pickle_file_path)
-                all_features.append(features[subject_id])
-                                                                                       
-            training_results = train(all_features, labels)
-            if SAVE_PICKLE_FILE:
-                iter_ = str(window_size) + 'ms_hc_cw_' + OUTPUT_PICKLE_NAME
-                output_pickle_file_path = os.path.join(OUTPUT_DIR, iter_)
-                utils.pickle_data(data=training_results, file_path=output_pickle_file_path)
+                    if SAVE_PICKLE_FILE:
+                        iter_ = subject_id + "_" + str(i) + '_' + str(window_size) + 'ms_hc_cw_' + OUTPUT_PICKLE_NAME
+                        output_pickle_file_path = os.path.join(OUTPUT_DIR, iter_)
+                        utils.pickle_data(data=results, file_path=output_pickle_file_path)
+
+
+    sys.path.remove(str(_parentdir))
